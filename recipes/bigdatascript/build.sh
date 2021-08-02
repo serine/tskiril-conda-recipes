3e1527829cba480533f6eb0b81964d63b06b4500
serine <tskiril@gmail.com>
2021-08-02 10:55:27 +1000

    work in progress on bds conda recipe

--- recipes/bigdatascript/build.sh
+++ recipes/bigdatascript/build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-bds_version=${PKG_VERSION}
-BDS_HOME="${PREFIX}/opt/bds-${bds_version}" scripts/install.sh
+java_url="https://github.com/AdoptOpenJDK/openjdk16-binaries/releases/download/jdk16u-2021-05-08-12-45/OpenJDK16U-jdk_x64_linux_hotspot_2021-05-08-12-45.tar.gz"
+jdk_fn="jdk_latest.tar.gz"
+wget -O ${jdk_fn} ${java_url}
+tar -zxf ${jdk_fn}
 
-mkdir -p "${PREFIX}/bin"
-mkdir -p "${PREFIX}/opt/bds-${bds_version}"
-ln -s "${PREFIX}/opt/bds-${bds_version}/bds" "${PREFIX}/bin/bds"
-ln -s "${PREFIX}/opt/bds-${bds_version}/bds.config" "${PREFIX}/bin/bds.config"
-chmod 0755 "${PREFIX}/opt/bds-${bds_version}/bds"
+#TODO note sure how to get that information from the tarbal
+java_home="jdk-16.0.1+9"
+
+bds_version=${PKG_VERSION}
+#BDS_HOME="${PREFIX}/opt/bds-${bds_version}" scripts/install.sh
+JAVA_HOME=${java_home} scripts/install.sh
+
+#mkdir -p "${PREFIX}/bin"
+#mkdir -p "${PREFIX}/opt/bds-${bds_version}"
+#ln -s "${PREFIX}/opt/bds-${bds_version}/bds" "${PREFIX}/bin/bds"
+#ln -s "${PREFIX}/opt/bds-${bds_version}/bds.config" "${PREFIX}/bin/bds.config"
+#chmod 0755 "${PREFIX}/opt/bds-${bds_version}/bds"
